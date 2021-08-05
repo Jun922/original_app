2e67caa9af3439f376aea7fc424759db0c3132e7
Jun922 <junjunchi620@gmail.com>
2021-08-05 21:01:37 +0900

    view,css

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
     validates :nickname
     validates :introduction, length: { maximum: 100 }
     validates :birthday
