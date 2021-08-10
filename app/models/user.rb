7268ce167cb6f95474f49966c9f17e2a376b91ec
Jun922 <junjunchi620@gmail.com>
2021-08-10 20:50:37 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :categories
-
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
+       
   with_options presence: true do
     validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
     validates :nickname
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "categories#index"
 
   resources :categories, only: [:index]
+  resources :rooms, only: [:index, :new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
