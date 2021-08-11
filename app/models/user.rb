066ffbebe7a5d5b6f124664f35eb9b563dbc13e2
Jun922 <junjunchi620@gmail.com>
2021-08-11 17:49:18 +0900

    db fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :room_users
-  has_many :rooms, through: :room_users
+  has_many :rooms
   has_many :messages
        
   with_options presence: true do
