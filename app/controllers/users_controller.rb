d2b05820895ca1b30e8acd8efd2f74e88f2707b8
Jun922 <junjunchi620@gmail.com>
2021-08-14 21:37:51 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      root to: "users#show"
     else
       render :edit
     end
