b86c5a2b5530e4a524d4599bd95c50b98fa79161
Jun922 <junjunchi620@gmail.com>
2021-08-26 00:06:00 +0900

    id

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :personals
   has_many :chats
+  
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
