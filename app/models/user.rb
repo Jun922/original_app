7aa5058853054a22149fc99e61e4230619ab7804
Jun922 <junjunchi620@gmail.com>
2021-08-30 00:28:34 +0900

    destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :posts
   
   has_many :room2s
-  has_many :chats
+  has_many :chats, dependent: :destroy
 
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
