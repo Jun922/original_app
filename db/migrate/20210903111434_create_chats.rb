85b29034b61cd0aae693328a7bee5d15858c16f0
Jun922 <junjunchi620@gmail.com>
2021-09-12 22:55:13 +0900

    binding.pry

--- db/migrate/20210903111434_create_chats.rb
+++ db/migrate/20210903111434_create_chats.rb
@@ -2,7 +2,7 @@ class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
       t.string :content
-      t.references :room, foreign_key: true
+      t.references :room2, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
     end
